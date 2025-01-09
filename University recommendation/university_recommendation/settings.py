b77a46ed33ff29a829e7e8b5676d666767c5343a
AdityaSingh234892 <160104818+AdityaSingh234892@users.noreply.github.com>
2025-01-09 23:31:06 +0530

    add some change for static

--- University recommendation/university_recommendation/settings.py
+++ University recommendation/university_recommendation/settings.py
@@ -57,8 +57,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")#os.environ.get("ALLOWED_HOST").split(" ")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")#os.environ.get("ALLOWED_HOST").split(" ") 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Application definition
 
